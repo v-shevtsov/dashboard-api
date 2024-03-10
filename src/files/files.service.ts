eb2cfd34093b4c54cd9eefcd20b8d08b3bc05cd4
v.shevtsov <v.shevtsov@smartweb.com.ua>
2024-03-10 11:31:52 +0100

    Upload local files

---
+++ src/files/files.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+import { FileElementResponse } from './dto/file-response-element.response';
+import { format } from 'date-fns';
+import { path } from 'app-root-path';
+import { ensureDir, writeFile } from 'fs-extra';
+
+@Injectable()
+export class FilesService {
+  async saveFiles(
+    files: Express.Multer.File[],
+  ): Promise<FileElementResponse[]> {
+    const dateFolder = format(new Date(), 'yyyy-MM-dd');
+    const uploadFolder = `${path}/uploads/${dateFolder}`;
+    await ensureDir(uploadFolder);
+    const response: FileElementResponse[] = [];
+
+    for (const file of files) {
+      await writeFile(`${uploadFolder}/${file.originalname}`, file.buffer);
+      response.push({
+        url: `${dateFolder}/${file.originalname}`,
+        name: file.originalname,
+      });
+    }
+
+    return response;
+  }
+}
